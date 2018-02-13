419046a628e4a9537143f44970d3a99bf0585960
Andrey N. Ronin <anronin@gmail.com>
2018-02-13 12:51:45 +0400

    Update poison

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Kogia.Mixfile do
       {:ex_doc, "~> 0.11.4", only: :docs},
       {:earmark, "~> 0.2.1", only: :docs},
       {:credo, "~> 0.3", only: [:dev, :test]},
-      {:poison, "~> 2.0"},
+      {:poison, "~> 3.0"},
       {:httpoison, "~> 0.9"}
     ]
   end
